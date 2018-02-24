58bbd1389002b618ddea07b44f40a345f14ce5d4
ultimatum056 <30701934+ultimatum056@users.noreply.github.com>
2018-02-24 20:23:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,20 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-   res.sendFile('artice one will be served here');
-});
-
-app.get('/article-two',function(req,res){
-   res.send('artice two one will be served here');
-});
-
-app.get('/article-three',function(req,res){
-   res.send('artice three will be served here');
-});
 
 app.get('/ui/style.css', function (req, res) {
-  res.send(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
