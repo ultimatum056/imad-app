da66c88dfd31019e12234e34f5ff3bf2decabcfd
ultimatum056 <30701934+ultimatum056@users.noreply.github.com>
2018-03-07 00:02:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile('article one will be served here ');
+  res.send('article one will be served here ');
 });
 
 app.get('/ui/style.css', function (req, res) {
