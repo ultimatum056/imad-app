e2e0da3e0c7d8d75632736823f57864248fcc79b
ultimatum056 <30701934+ultimatum056@users.noreply.github.com>
2018-03-07 16:49:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles={
-    'article-one':{
+    'articleone':{
         title :'article-one',
         heading:'articleOne',
         content:` <h1>Hello</h1>
