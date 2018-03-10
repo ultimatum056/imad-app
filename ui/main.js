40a348b73718399d563f80d77d6a5661abb1d111
ultimatum056 <30701934+ultimatum056@users.noreply.github.com>
2018-03-10 22:07:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,11 +9,11 @@ var img = document.getElementById('img');
 var marginLeft = 0 ;
 
 function moveRight(){
-    marginLeft = marginLeft + 2;
+    marginLeft = marginLeft + 3;
     img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function(){
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,90);
 };
 
