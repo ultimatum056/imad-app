dd68fc826da3ec0b2b82c66789ccf4a6e9831f81
ultimatum056 <30701934+ultimatum056@users.noreply.github.com>
2018-03-11 01:42:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,7 +36,7 @@ submit.onclick =function(){
     
     //mkae a req to server and send name
     //capture
-    var names=['name1','namne2','name3'];
+    var names=['name1','namne2','name3','name 4'];
     var list='';
     for(var i=0;i<names.length;i++){
         list +='<li>' + names[i] + '</li>';
