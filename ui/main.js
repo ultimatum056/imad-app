20ab6cf501273b0c502f5d6c4173954bea4a14d9
ultimatum056 <30701934+ultimatum056@users.noreply.github.com>
2018-03-10 21:38:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,6 @@
 console.log('Loaded!');
 
-var element = document.getElementById('ak');
-element.innnerHTML = 'the text has changed';
+
 //move the img
 var img = document.getElementById('img');
 img.onclick = function(){
