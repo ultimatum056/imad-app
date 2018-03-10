8b7852b397b184eeb26b281bd15e7556a2986954
ultimatum056 <30701934+ultimatum056@users.noreply.github.com>
2018-03-10 22:02:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,15 @@ ele.innerHTML='new vaalue';
 
 //move the img
 var img = document.getElementById('img');
+
+var marginLeft =0 ;
+
+function moveRight(){
+    marginLeft = marginLeft +'10';
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick = function(){
-    img.style.marginLeft='100px';
+    var interval = setInterval(moveRight,100);
 };
 
